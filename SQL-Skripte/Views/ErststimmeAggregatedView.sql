ff02c8f142bc2ed534539e86c46282dd981814c1
MarkusSchnappi <markus.schnappinger@gmx.de>
2015-12-03 14:09:24 +0100

    added comments for maintainability

--- SQL-Skripte/Views/ErststimmeAggregatedView.sql
+++ SQL-Skripte/Views/ErststimmeAggregatedView.sql
@@ -1,4 +1,7 @@
-Create View ErststimmeAggregated as
+alter View ErststimmeAggregated as
+
+
+/* used for special mode: comment first command out if computated based on single votes is demanded, otherwise use first function*/
 
 -- Based on already aggregated table
 select Election_Id, Wahlkreis_Id, Person_Id, Amount from ErststimmeAmount
--- SQL-Skripte/Views/ZweitstimmeAggregatedView.sql
+++ SQL-Skripte/Views/ZweitstimmeAggregatedView.sql
@@ -1,5 +1,7 @@
 Create View ZweitstimmeAggregated as
 
+/*used for special mode: select data based on single votes: open this view, comment first function out, un-comment second function*/
+
 -- Based on already aggregated table
 select Election_Id, Party_Id, Wahlkreis_Id, Amount from ZweitstimmeAmount
 
